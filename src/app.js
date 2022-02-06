ea1696c9eb8c16dbe53ab7a3f220419b51c2d17f
tomassm11 <tomas.salcedo@cesjuanpablosegundo.es>
2022-02-06 20:51:01 +0100

    Update
    Ahora si funciona

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
 import express from "express";
-import exphbs from "express-handlebars";
+import {create} from "express-handlebars";
+
 import indexRoutes from "./routes/index.routes";
 import path from "path";
 
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
-app.engine(
-  ".hbs",
-  exphbs({
-    layoutDir: path.join(app.get("views"), "layout"),
-    defaultLayout: "main",
-    extname: ".hbs",
-  })
-);
+
+const exphbs = create({
+  extname: '.hbs',
+  layoutsDir: path.join(app.get("views"), "layout"),
+  partialsDir: path.join(app.get("views"), "partials"),
+  defaultLayout:'main'
+});
+
+app.engine(".hbs", exphbs.engine);
 app.set("view engine", ".hbs");
 
-//Las rutas
 app.use(indexRoutes);
 
 export default app;
