1476eb5244d262e87a925f11510d0e488c040d07
tomassm11 <tomas.salcedo@cesjuanpablosegundo.es>
2022-02-12 17:06:09 +0100

    update

--- dist/routes/index.routes.js
+++ dist/routes/index.routes.js
@@ -19,9 +19,19 @@ var _usuario = require("../controllers/usuario.controllers");
 
 var router = (0, _express.Router)(); //Renderizado de nuestras rutas
 
+router.get("/", _usuario.renderInicio);
+router.get("/", _usuario.renderDefault);
 router.get("/", _usuario.renderUsuarios);
+router.get("/", _usuario.renderEscenarios);
+router.get("/", _usuario.renderSuperheroes);
 router.post("/Usuario/add", _usuario.anadirUsuario);
-router.get("/Formulario", _usuario.paginaFormulario);
+router.post("/Escenario/add", _usuario.anadirEscenario);
+router.post("/Superheroe/add", _usuario.anadirSuperheroe);
+router.get("/inicio", _usuario.paginaInicioPrincipal);
+router.get("/index", _usuario.paginaInicio);
+router.get("/formUser", _usuario.paginaFormularioUsuarios);
+router.get("/formEscenario", _usuario.paginaFormularioEscenarios);
+router.get("/formSuperheroe", _usuario.paginaFormularioSuperheroes);
 router.get("/personajes", _usuario.paginaPersonajes);
 var _default = router;
 exports["default"] = _default;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,13 +2,23 @@ import { Router } from "express";
 import req from "express/lib/request";
 import res from "express/lib/response";
 import Usuario from "../models/Usuario";
-import { anadirUsuario, paginaFormulario, paginaPersonajes, renderUsuarios } from "../controllers/usuario.controllers";
+import {paginaInicioPrincipal, anadirEscenario, anadirSuperheroe, anadirUsuario, paginaInicio, paginaFormularioEscenarios, paginaFormularioSuperheroes, paginaFormularioUsuarios, paginaPersonajes, renderDefault, renderEscenarios, renderSuperheroes, renderUsuarios, renderInicio } from "../controllers/usuario.controllers";
 const router = Router();
 
 //Renderizado de nuestras rutas
+router.get("/", renderInicio);
+router.get("/", renderDefault);
 router.get("/", renderUsuarios);
+router.get("/", renderEscenarios);
+router.get("/", renderSuperheroes);
 router.post("/Usuario/add", anadirUsuario);
-router.get("/Formulario", paginaFormulario);
+router.post("/Escenario/add", anadirEscenario);
+router.post("/Superheroe/add", anadirSuperheroe);
+router.get("/inicio", paginaInicioPrincipal);
+router.get("/index", paginaInicio);
+router.get("/formUser", paginaFormularioUsuarios);
+router.get("/formEscenario", paginaFormularioEscenarios);
+router.get("/formSuperheroe", paginaFormularioSuperheroes);
 router.get("/personajes", paginaPersonajes);
 
 
